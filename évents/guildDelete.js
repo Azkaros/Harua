a243502bd76dd8e8c0dd50bb00d5b3c44987cc32
Nikolaï <32989498+Azkaros@users.noreply.github.com>
2019-06-03 20:53:12 +0200

    Update guildDelete.js

--- évents/guildDelete.js
+++ évents/guildDelete.js
@@ -6,4 +6,8 @@ module.exports = async (client, guild) => {
     console.log(`J'ai quitté: ${guild.name} (id: ${guild.id}). Il avait ${guild.memberCount} membres!`);
     client.user.setPresence({ game: { name: `h!help | ${client.guilds.size} | ${client.users.size}` }, status: 'online' });
     client.user.setGame(`h!help | aide ${client.guilds.size} serveurs !`, 'https://www.twitch.tv/nagitoyourgoodboy');
+    
+    const channel = client.channels.find('name', "【🌸】join-leave-server-bot-notif")
+    
+    channel.send(`J'ai quitté: ${guild.name} (id: ${guild.id}). Il avait ${guild.memberCount} membres!`);
 }
