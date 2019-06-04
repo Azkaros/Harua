1373e7a93e86a88b2eae967ac1b9078ab531f332
Nikolaï <32989498+Azkaros@users.noreply.github.com>
2019-06-04 14:41:56 +0200

    Update guildDelete.js

--- évents/guildDelete.js
+++ évents/guildDelete.js
@@ -4,6 +4,6 @@ const client = new Discord.Client();
 
 module.exports = async (client, guild) => {
     console.log(`J'ai quitté: ${guild.name} (id: ${guild.id}). Il avait ${guild.memberCount} membres!`);
-    client.user.setPresence({ game: { name: `h!help | ${client.guilds.size} | ${client.users.size}` }, status: 'online' });
-    client.user.setGame(`h!help | aide ${client.guilds.size} serveurs !`, 'https://www.twitch.tv/nagitoyourgoodboy');
+  client.user.setPresence({ game: { name: `h!help | aide ${client.guilds.size} serveurs !` }, status: 'online' });
+  client.user.setGame(`h!help | aide ${client.guilds.size} serveurs !`, 'https://www.twitch.tv/nagitoyourgoodboy');
 }
