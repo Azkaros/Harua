a2a619881e2d841e5f9c40a74362cdcffcedc86e
Nikolaï <32989498+Azkaros@users.noreply.github.com>
2019-06-04 14:41:49 +0200

    Update guildCreate.js

--- évents/guildCreate.js
+++ évents/guildCreate.js
@@ -4,6 +4,6 @@ const client = new Discord.Client();
 
 module.exports = async (client, guild) => {
     console.log(`Nouveau serveur rejoint: ${guild.name} (id: ${guild.id}). Il a ${guild.memberCount} membres!`);
-    client.user.setPresence({ game: { name: `h!help | ${client.guilds.size} | ${client.users.size}` }, status: 'online' });
-    client.user.setGame(`h!help | aide ${client.guilds.size} serveurs !`, 'https://www.twitch.tv/nagitoyourgoodboy');
+  client.user.setPresence({ game: { name: `h!help | aide ${client.guilds.size} serveurs !` }, status: 'online' });
+  client.user.setGame(`h!help | aide ${client.guilds.size} serveurs !`, 'https://www.twitch.tv/nagitoyourgoodboy');
 }
